da88f3c503555299fa7031ec4572356b1fb27e0a
Rickey Morris <rmorris.rickey@gmail.com>
2022-09-15 16:58:50 -0400

    Changed the background color of the body and the images. Also, removed the background from each of the images using gimp so that a color of my choosing can now show behind each hand. Used flexbox to center items.

--- myScript.js
+++ myScript.js
@@ -17,55 +17,52 @@ function playGame(computerChoice, playerChoice) {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
         outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return true;
     }
     else if (computerChoice == 'Rock' && playerChoice == 'Paper') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
         outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return false;
     }
     else if (computerChoice == 'Paper' && playerChoice == 'Rock') {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
         outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return true;
     }
     else if (computerChoice == 'Paper' && playerChoice == 'Scissors') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
         outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return false;
     }      
     else if (computerChoice == 'Scissors' && playerChoice == 'Paper') {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
         outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return true;
     }
     else if (computerChoice == 'Scissors' && playerChoice == 'Rock') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
         outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
-        return false;
     }
 }
 
 function getPlayerChoice() {
-    event.stopImmediatePropagation();
         playGame(getComputerChoice(), event.currentTarget.name);//currentTarget.name retrieves the name attribute of the element that has the EV attached.
-        console.log(event.currentTarget.name);
         if (playerScore == 5 || computerScore == 5) {
             end = document.createElement('div');
-            end.innerText = `Game Over! ${(playerScore > computerScore) ? "Player" : "Computer"} Wins!`;
-            document.body.appendChild(end);
-}
+            end.innerText = `Game Over! ${(playerScore > computerScore) ? "You Win!" : "Computer Wins!"}`;
+            document.querySelector('.main').appendChild(end);
+            for (const button of buttons) {
+                button.disabled = true;
+            }
+            playerScore = 0;
+            computerScore = 0;
+    }
 }
 let computerScore = 0;
 let playerScore = 0;
 
 const buttons = document.getElementsByTagName("button");
-const outputDiv = document.querySelector("div");
+const outputDiv = document.querySelector(".output");
 outputDiv.innerText = `Computer: ${computerScore} Player: ${playerScore}`;
 
 for (const button of buttons) {
