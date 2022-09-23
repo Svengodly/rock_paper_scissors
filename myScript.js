46effe2b7733065188783b766952af18f21f1e9a
Rickey Morris <rmorris.rickey@gmail.com>
2022-09-23 13:54:47 -0400

    Added flex containers that display the player's and computer's score on the left and right sides respectively. Placed CSS transitions on these containers that increase the font-size and change the background color to green of the victor. Added margin-right to each of the buttons. Added <p> element, which notifies the player that the game is first to five.

--- myScript.js
+++ myScript.js
@@ -10,43 +10,72 @@ function getComputerChoice() {
 function playGame(computerChoice, playerChoice) {
     //This fucntion needs to return a value, which will be used to track points.
     if (computerChoice == playerChoice) {
-        console.log(`Draw! Computer: ${computerChoice} Player: ${playerChoice}`)
-        outputDiv.innerText = `Draw! Computer: ${computerChoice} Player: ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        console.log(`Draw! Computer: ${computerChoice} Player: ${playerChoice}`);
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `Draw!`;
+        return null;
     }
     else if (computerChoice == 'Rock' && playerChoice == 'Scissors') {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
-        outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You lose! ${computerChoice} beats ${playerChoice}`;
+        return true;
     }
     else if (computerChoice == 'Rock' && playerChoice == 'Paper') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
-        outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You win! ${computerChoice} loses to ${playerChoice}`;
+        return false;
     }
     else if (computerChoice == 'Paper' && playerChoice == 'Rock') {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
-        outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You lose! ${computerChoice} beats ${playerChoice}`;
+        return true;
     }
     else if (computerChoice == 'Paper' && playerChoice == 'Scissors') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
-        outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You win! ${computerChoice} loses to ${playerChoice}`;
+        return false;
     }      
     else if (computerChoice == 'Scissors' && playerChoice == 'Paper') {
         console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
         ++computerScore;
-        outputDiv.innerText = `You lose! ${computerChoice} beats ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You lose! ${computerChoice} beats ${playerChoice}`;
+        return true;
     }
     else if (computerChoice == 'Scissors' && playerChoice == 'Rock') {
         console.log(`You win! ${computerChoice} loses to ${playerChoice}`);
         ++playerScore;
-        outputDiv.innerText = `You win! ${computerChoice} loses to ${playerChoice}\nComputer: ${computerScore} Player: ${playerScore}`;
+        playerOutput.innerText = `Player: ${playerScore}`;
+        computerOutput.innerText = `Computer: ${computerScore}`;
+        result.innerText = `You win! ${computerChoice} loses to ${playerChoice}`;
+        return false;
     }
 }
 
 function getPlayerChoice() {
-        playGame(getComputerChoice(), event.currentTarget.name);//currentTarget.name retrieves the name attribute of the element that has the EV attached.
+        outcome = playGame(getComputerChoice(), event.currentTarget.name);//currentTarget.name retrieves the name attribute of the element that has the EV attached.
+        if (outcome === null){
+            console.log("NULLLL")
+        } else if (outcome) {
+            computerOutput.classList.add("win");
+        } else if(!outcome) {
+            playerOutput.classList.add("win");
+        } 
+
         if (playerScore == 5 || computerScore == 5) {
             end = document.createElement('div');
             end.innerText = `Game Over! ${(playerScore > computerScore) ? "You Win!" : "Computer Wins!"}`;
@@ -57,17 +86,39 @@ function getPlayerChoice() {
             playerScore = 0;
             computerScore = 0;
     }
+    outcome = null;
 }
+
 let computerScore = 0;
 let playerScore = 0;
+let outcome = null;
 
 const buttons = document.getElementsByTagName("button");
-const outputDiv = document.querySelector(".output");
-outputDiv.innerText = `Computer: ${computerScore} Player: ${playerScore}`;
+const outputDiv = document.querySelectorAll(".output");
+const playerOutput = document.querySelector(".output.player");
+const computerOutput = document.querySelector(".output.computer");
+const result = document.querySelector(".result");
+
+playerOutput.addEventListener("transitionend", () => {
+    playerOutput.classList.remove("win");
+})
+
+computerOutput.addEventListener("transitionend", () => {
+    computerOutput.classList.remove("win");
+})
+
+for(const x of outputDiv) {
+    if(x.className == "output player"){
+        x.innerText = `Player: ${playerScore}`;
+        // x.innerText = `Computer: ${computerScore} Player: ${playerScore}`;
+} else {
+    x.innerText = `Computer: ${computerScore}`;
+}
+//outputDiv.innerText = `Computer: ${computerScore} Player: ${playerScore}`;
 
 for (const button of buttons) {
     button.addEventListener("click", getPlayerChoice);
         }
 
-
+    }
 
